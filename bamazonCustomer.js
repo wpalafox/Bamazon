0847d450b9f9a2e44b68e47180d2f5e88911ed2e
Will Palafox <wpalafox@gmail.com>
2016-12-05 20:50:58 -0600

    Called prompt function to include the response as a parameter

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection({
   // Your password
   password: "",
   database: "bamazon_db"
+
 });
 
 
@@ -46,12 +47,12 @@ connection.connect(function(err){
 // table is an Array, so you can `push`, `unshift`, `splice` and friends 
 
 
-//Table generator 
+//Prints the table from the mySQL database 
 function list(table){
 
-      connection.query("SELECT * FROM " + table, function(err, res) {
+      connection.query("SELECT * FROM " + table, function(err, res){
       
-		var table = new Table({ 
+		var tableDisplay = new Table({ 
 
 			head: ['Item', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
 		  	,colWidths: [6, 18, 18, 10, 17]
@@ -65,7 +66,7 @@ function list(table){
 
       		for(i=0;i<res.length;i++){
 
-      		table.push(
+      		tableDisplay.push(
     
 			    [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity] 
 			
@@ -73,52 +74,79 @@ function list(table){
       
       	}
 
-      		console.log(table.toString());
-      		console.log("Press the Up Arrow");
+      		console.log(tableDisplay.toString());
+      		//calls the prompt user function and takes the response as an argument/parameter
+      		
+      		promptUsers(res);
+      		
 
+		
 	});
      
 };
 
 
 
-function promptUsers(){
 
-	inquirer.prompt([
+
+function updateTable
+
+
+
+//Prompts users for the specific item they want, and how many units
+function promptUsers(res){
+
+	
+		inquirer.prompt([
 
 		  	// Here we give the user a list to choose from.
 		  {
 		    
 		    type: "input",
-		    message: "Enter an Item ID you would like to buy?",
-		    name: "itemID"
-		  
+		    message: "Enter an Item ID you would like to buy",
+		    name: "item"
 		  
 		  },
 		
 		  {
 
-		  	
 		  	type: "input",
-		  	message: "How many units would you like to buy?",
-		  	name: "unitsNumber"
-
-
+		  	message: "How many units?",
+		  	name: "howMany"
+		
 		  }
 
 
 
 
-		]).then(function(user) {
+		]).then(function(userInput) {
 
 			// If we log that user as a JSON, we can see how it looks.
-			 console.log(JSON.stringify(user, null, 2));
-		
+			 //console.log(JSON.stringify(userInput));
 
-		
+			//Sets variable to user selected item ID 
+			var itemID = userInput.item;
+			console.log("itemID: "+userInput.item);
+
+			var amount = res[itemID - 1].stock_quantity;
+			amount = amount - 1;
+			console.log("new amount: "+amount);
+			
+			connection.query("UPDATE storefront SET item_id = ?",amount,function (error,resHere){
+				//	if (error) throw error;
+				console.log("in the update function");
+			
+			});
+			
+			
+
+			
+			
 
 
 
+
+			//console.log(res[itemID].stock_quantity);
 		
 
 
@@ -131,15 +159,94 @@ function promptUsers(){
 };
 
 
+/*
+
+connection.query("SELECT * FROM storefront", function(err2, upRes){
+      
+			var tableDisplayOuter = new Table({ 
+
+			head: ['Item', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
+		  	,colWidths: [6, 18, 18, 10, 17]
+
+				});
 
 
+			//if (err2) throw err2;
+        
+        	// table is an Array, so you can `push`, `unshift`, `splice` and friends 
+
+      		for(i=0;i<upRes.length;i++){
+
+      		tableDisplayOuter.push(
+    
+			    [upRes[i].item_id, upRes[i].product_name, upRes[i].department_name, upRes[i].price, upRes[i].stock_quantity] 
+			
+			);
+      
+      		}
+
+			console.log(tableDisplayOuter.toString());      		
+      		console.log("Stock Remaining: "+upRes[itemID-1].stock_quantity);
+
+      		});
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function testFunction(){ 
+
+	connection.query(
+				  
+	'UPDATE storefront SET stock_quantity = ? Where item_id = ?',
+	
+	[49, 3],
+//Need to make response global
+
+		function (err, res){
+
+			
+			if (err) throw err;
+			
+			console.log('Changed ' + res.changedRows + ' rows');
+				 
+		
+		}); 
+	
+
+};  
+
+*/
+
+
+
+
+
+
+
+//testFunction();
 list('storefront');
-promptUsers();
+
+//promptUsers();
+
 connection.end();
 
 
 /*
-
 inquirer.prompt([
 
 
@@ -148,9 +255,7 @@ inquirer.prompt([
 		message: "What is the ID of the product you would like to by?"
 		productID: "productID"
 
-
 	},
-
 }])
 
 */
